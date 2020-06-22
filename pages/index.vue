b0170b4b396d7e5fd74531daad79821fdc93aa88
RainBoi <max.erebus@gmail.com>
2020-06-22 14:48:22 +0400

    init w setup

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="page-container">
+        <h1>Hub Georgia - ჰაბ ჯორჯია</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index"
+};
+</script>
+
+<style scoped>
+</style>
