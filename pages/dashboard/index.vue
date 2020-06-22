4a594815490a1b3c713ead93ff46f8940c32a0fa
RainBoi <max.erebus@gmail.com>
2020-06-22 16:31:11 +0400

    Added dashboard page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="dashboard">
+        <h1>Hub Georgia Dashboard</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    middleware: "authenticated",
+    name: "Dashboard",
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped></style>
