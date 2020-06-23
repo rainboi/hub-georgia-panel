2fc0b51cad9facda39b8eb0d1f8506c4fae54336
RainBoi <max.erebus@gmail.com>
2020-06-23 05:22:25 +0400

    Added navigation vuex module

---
+++ store/navigation.js
@@ -0,0 +1,53 @@
+const state = () => ({
+    all: []
+});
+
+const getters = {
+    all: state => state.all
+};
+
+const actions = {
+    async fetch({ commit }, langID = 1) {
+        try {
+            const response = await this.$axios.get(
+                `navigation/fetch?lang_id=${langID}`
+            );
+            await commit("setAll", response.data.result.data);
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    },
+
+    async add(params) {
+        try {
+            const response = await this.$axios.post(`navigation/add`, params);
+            let data = response.data.result.data;
+            return data ? true : false;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    },
+    async delete(ID) {
+        try {
+            const response = await this.$axios.post(`navigation/delete/${ID}`);
+            let data = response.data.result.data;
+            return data ? true : false;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+};
+
+const mutations = {
+    setAll: (state, data) => (state.all = data)
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
